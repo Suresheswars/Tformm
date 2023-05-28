1fec6c800011e01bff136b9a9c70a76e31d85222
Suresh <sureshcse315@gmail.com>
2023-05-28 14:49:33 +0530

    Removed few files

--- acc/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-amiid = "ami-0573b70afecda915d"
-type  = "t2.medium"
--- pro/main.tf
+++
@@ -1,8 +0,0 @@
-module "instance_creation" {
-  source = "../modules"
-  sgname = var.sgname
-  sshport = var.sshport
-  amiid = var.amiid
-  env = var.env
-  type = var.type
-}
