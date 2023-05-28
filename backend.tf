b246d18f33eecaef1c7917ffa10c13947e0157d0
Suresh <sureshcse315@gmail.com>
2023-05-28 16:14:23 +0530

    backed files are amended

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "devopstechi"
+    //bucket         = "devopstechi"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "devopsteche"
+    //dynamodb_table = "devopsteche"
     access_key = "AKIAYNYJGFY7FMRDBMHI"
     secret_key = "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"
   }
