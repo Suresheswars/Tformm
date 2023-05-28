814a18cd15bed54a58907d4ba38a0b92ffeca5eb
Suresh <sureshcse315@gmail.com>
2023-05-28 16:12:47 +0530

    backed file amended

--- backend.tf
+++ backend.tf
@@ -4,5 +4,7 @@ terraform {
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "devopsteche"
+    access_key = "AKIAYNYJGFY7FMRDBMHI"
+    secret_key = "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"
   }
 }
